9c96b79ef6dc22b7a7c8c4e271a5e6e753d7e9a8
linwei0045 <linwei0045@zentech-inc.com>
2018-03-16 10:55:38 +0800

    git second

---
+++ src/test/java/test.java
@@ -0,0 +1,11 @@
+/**
+ * Zentech-Inc
+ * Copyright (C) 2018 All Rights Reserved.
+ */
+
+/**
+ * @author lw0045
+ * @version $Id test.java, v 0.1 2018-03-16 10:54 lw0045 Exp $$
+ */
+public class test {
+}
